3044a8688159685f9b21de8ca2a729200941b8ab
Jyates92 <jeremiahyates1992@gmail.com>
2020-12-08 20:28:11 -0800

    Cannot find ./Components

--- README.md
+++
@@ -1,2 +0,0 @@
-# Auth0BoilerPlate
-This will be a boiler plate auth0 for my future endeavors utilizing auth0
--- auth0boilerplate/src/App.js
+++ auth0boilerplate/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { NavBar, Footer, Loading } from "./components";
+import { Route, Switch } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
-function App() {
+
+import { Home, Profile, ExternalApi } from "./views";
+
+import "./app.css";
+
+const App = () => {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app" className="d-flex flex-column h-100">
+      <NavBar />
+      <div className="container flex-grow-1">
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/external-api" component={ExternalApi} />
+        </Switch>
+      </div>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
