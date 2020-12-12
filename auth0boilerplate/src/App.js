b1d1b0e6e4edfcc7ce6563a33a676d968da71a08
Jyates92 <jeremiahyates1992@gmail.com>
2020-12-11 18:06:32 -0800

    updated index and app.js

--- auth0boilerplate/src/App.js
+++ auth0boilerplate/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { NavBar, Footer, Loading } from "./components";
 import { Route, Switch } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 
-
+import { NavBar, Footer, Loading } from "./components";
 import { Home, Profile, ExternalApi } from "./views";
+import ProtectedRoute from "./auth/protected-route";
 
 import "./app.css";
 
@@ -21,8 +21,8 @@ const App = () => {
       <div className="container flex-grow-1">
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/profile" component={Profile} />
-          <Route path="/external-api" component={ExternalApi} />
+          <ProtectedRoute path="/profile" component={Profile} />
+          <ProtectedRoute path="/external-api" component={ExternalApi} />
         </Switch>
       </div>
       <Footer />
